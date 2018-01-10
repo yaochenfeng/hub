4f3daed20360e8074ec3633a49e862930405c5e8
yaochenfeng <yaochenfeng@bestws.cn>
2018-01-10 15:43:31 +0800

    add

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,18 @@ Route::get('/passport', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id' => '2',
+        'redirect_uri' => 'http://127.0.0.1:8000/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect('http://127.0.0.1:8000/oauth/authorize?'.$query);
+});
+
+Route::get('download',function(){
+
+    return response()->download(realpath(base_path('public')).'/医学人工智能联盟申请表.doc', '医学人工智能联盟申请表.doc');
+});
