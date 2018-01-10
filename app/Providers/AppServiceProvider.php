0d886acc06a7850fa294c160c0b96b5c37b1bb28
yaochenfeng <yaochenfeng@bestws.cn>
2018-01-10 10:56:42 +0800

    passport add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+
+        Passport::ignoreMigrations();
     }
 }
